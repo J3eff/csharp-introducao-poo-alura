12421d44ea111dca3f675d78942d97af283a3ac6
J3eff <jeffersonvideo125@gmail.com>
2022-04-04 23:47:18 -0300

    Produtividade com Get e Set

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,52 @@
+﻿
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get { return _saldo; }
+
+            set
+            {
+                if (value < 0)
+                    return; //Para de executar o método, e devolve o fluxo do programa.
+
+                _saldo = value;
+            }
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+                return false;
+
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente constaDestino)
+        {
+            if (_saldo < valor)
+                return false;
+
+            _saldo -= valor;
+            constaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
+
