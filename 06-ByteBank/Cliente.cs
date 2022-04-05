84880f0a6223bf8d345f3e7c31ccf55ecf13c944
J3eff <jeffersonvideo125@gmail.com>
2022-04-04 22:51:56 -0300

    ObterSaldo e DefinirSaldo

---
+++ 06-ByteBank/Cliente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    public class Cliente
+    {
+        public string nome;
+        public string cpf;
+        public string profissao;
+    }
+}
---
+++ 06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,49 @@
+﻿
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        private double saldo = 100;
+
+        public void DefinirSaldo(double saldo)
+        {
+            if (saldo < 0)
+                return; //Para de executar o método, e devolve o fluxo do programa.
+
+            this.saldo = saldo;
+        }
+
+        public double ObterSaldo()
+        {
+            return saldo;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+                return false;
+
+            saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente constaDestino)
+        {
+            if (saldo < valor)
+                return false;
+
+            saldo -= valor;
+            constaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
+
